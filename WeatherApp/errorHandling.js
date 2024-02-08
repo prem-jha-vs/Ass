30d02271b7a54e804829bf67ddb51beab0c6a8f3
prem-jha-vs <157302866+prem-jha-vs@users.noreply.github.com>
2024-02-08 10:34:39 +0530

    weather

--- WeatherApp/errorHandling.js
+++
@@ -1,8 +0,0 @@
-function showError() {
-    document.querySelector(".error").style.display = "block";
-    document.querySelector(".weather").style.display = "none";
-}
-
-function hideError() {
-    document.querySelector(".error").style.display = "none";
-}
